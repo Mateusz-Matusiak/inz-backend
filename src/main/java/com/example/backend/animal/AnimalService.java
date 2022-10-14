d0a57ca8b7f70b057b304fa6ab4596d285eedf35
Mateusz Matusiak <mateusz.matusiak@likims.com>
2022-10-14 14:48:48 +0200

    Add AnimalService to test Actions

---
+++ src/main/java/com/example/backend/animal/AnimalService.java
@@ -0,0 +1,12 @@
+package com.example.backend.animal;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class AnimalService {
+    private final AnimalRepository animalRepository;
+}
