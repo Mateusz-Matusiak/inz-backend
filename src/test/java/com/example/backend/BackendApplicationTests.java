140dbc2a404e8dfb0da0339e5b3f8c1b2d56a1e6
Mateusz Matusiak <mateusz.matusiak@likims.com>
2022-10-18 18:23:18 +0200

    Fix tests

--- src/test/java/com/example/backend/BackendApplicationTests.java
+++ src/test/java/com/example/backend/BackendApplicationTests.java
@@ -2,9 +2,25 @@ package com.example.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
 class BackendApplicationTests {
+
+	@Container
+	public static PostgreSQLContainer container = new PostgreSQLContainer("latest");
+
+	@DynamicPropertySource
+	public static void overrideProps(DynamicPropertyRegistry registry) {
+		registry.add("spring.datasource.url", container::getJdbcUrl);
+		registry.add("spring.datasource.username", container::getUsername);
+		registry.add("spring.datasource.password", container::getPassword);
+	}
 
 	@Test
 	void contextLoads() {
